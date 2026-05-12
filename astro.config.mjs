a56e14ec7c4c8f0610fe928821681daf5ce43adb
brightybaron <ardi.panda54@protonmail.com>
2026-05-12 20:33:41 +0700

    huhaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,13 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      external: [
+        "@prisma/client",
+        "@prisma/adapter-pg",
+        "@supabase/supabase-js",
+      ],
+    },
   },
 
   output: "server",
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,32 @@
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+
+// const connectionString = import.meta.env.DATABASE_URL;
+// const adapter = new PrismaPg({ connectionString });
+// // const prismaAdapter = new PrismaClient({ adapter });
+
+// // export default prisma;
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma?: PrismaClient;
+// };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+// if (import.meta.env.MODE !== "production") globalForPrisma.prisma = prisma;
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 const connectionString = import.meta.env.DATABASE_URL;
 const adapter = new PrismaPg({ connectionString });
-// const prismaAdapter = new PrismaClient({ adapter });
-
-// export default prisma;
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+  prisma?: InstanceType<typeof PrismaClient>;
 };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
-if (import.meta.env.MODE !== "production") globalForPrisma.prisma = prisma;
+if (import.meta.env.MODE !== "production") {
+  globalForPrisma.prisma = prisma;
+}

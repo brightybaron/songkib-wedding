b9afde92b2561aacc2291521de11981a2912e95f
brightybaron <ardi.panda54@protonmail.com>
2026-05-12 20:44:40 +0700

    huahea1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -15,28 +15,28 @@
 
 // if (import.meta.env.MODE !== "production") globalForPrisma.prisma = prisma;
 
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { PrismaClient } from "@prisma/client";
-
-// const connectionString = import.meta.env.DATABASE_URL;
-// const adapter = new PrismaPg({ connectionString });
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma?: InstanceType<typeof PrismaClient>;
-// };
-
-// export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-// if (import.meta.env.MODE !== "production") {
-//   globalForPrisma.prisma = prisma;
-// }
-
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
+const connectionString = import.meta.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+  prisma?: InstanceType<typeof PrismaClient>;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
-if (import.meta.env.MODE !== "production") globalForPrisma.prisma = prisma;
+if (import.meta.env.MODE !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma?: PrismaClient;
+// };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+// if (import.meta.env.MODE !== "production") globalForPrisma.prisma = prisma;
